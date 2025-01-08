572dda64674795fe4cb064df7f18139f70b1bfdc
yeferson rivas <yeferok2@gmail.com>
2025-01-08 15:57:15 -0500

    Add Firebase Cloud Functions and notification service for new orders

--- lib/src/data/datasource/notification_service_datasource.dart
+++ lib/src/data/datasource/notification_service_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -5,6 +7,8 @@ class NotificationServiceDatasource {
   final FlutterLocalNotificationsPlugin _localNotifications =
       FlutterLocalNotificationsPlugin();
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> initialize() async {
     const AndroidInitializationSettings initializationSettingsAndroid =
@@ -31,11 +35,33 @@ class NotificationServiceDatasource {
       badge: true,
       sound: true,
     );
-
+    await _saveFcmToken(userId);
+    _firebaseMessaging.onTokenRefresh.listen((String newToken) async {
+      await _saveFcmToken(userId);
+    });
     FirebaseMessaging.onMessage.listen(_onMessageHandler);
   }
 
+  String get userId => _auth.currentUser!.uid;
+
   Future<String?> get fmcToken async => await _firebaseMessaging.getToken();
+
+  Future<void> _saveFcmToken(String userId) async {
+    String? token = await fmcToken;
+    if (token != null) {
+      try {
+        await _firestore.collection('sellers').doc(userId).set(
+          <String, Object?>{
+            'fcmToken': token,
+            'lastUpdated': FieldValue.serverTimestamp(),
+          },
+          SetOptions(merge: true),
+        );
+      } catch (e) {
+        null;
+      }
+    }
+  }
 
   Future<void> _onMessageHandler(RemoteMessage message) async {
     final RemoteNotification? notification = message.notification;
@@ -53,8 +79,8 @@ class NotificationServiceDatasource {
   }) async {
     const AndroidNotificationDetails androidDetails =
         AndroidNotificationDetails(
-      'channel_id',
-      'channel_name',
+      'orders_chanel',
+      'orders',
       importance: Importance.high,
       priority: Priority.high,
     );
